831337016320d83b5a0b94a97683d7758443e844
Seongjoon Chu <chu@localhost.localdomain>
2016-01-06 22:23:06 +0900

    delete all files

--- README.md
+++
@@ -1,2 +0,0 @@
-# SSMCP15-2
-Samsung Software Software Membership Creative Project 2015_2
--- app/src/main/jni/HelloNDK.h
+++
@@ -1,8 +0,0 @@
-//
-// Created by chu on 12/21/15.
-//
-
-#ifndef JNITEST_HELLONDK_H
-#define JNITEST_HELLONDK_H
-
-#endif //JNITEST_HELLONDK_H
